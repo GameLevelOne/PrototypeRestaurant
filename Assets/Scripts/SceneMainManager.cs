8d189d8107594b09e89cad64602e2180c4a2b2d7
DennisGL1 <dennis@gamelevelone.com>
2018-05-22 15:21:18 +0700

    - add device orientation detection

---
+++ Assets/Scripts/SceneMainManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneMainManager : MonoBehaviour {
+	public GameObject landscapeDisp, portraitDisp;
+	public Text textOrientation;
+	public void ButtonLandscapeOnClick()
+	{
+		Screen.orientation = ScreenOrientation.Landscape;
+		landscapeDisp.SetActive(true);
+		portraitDisp.SetActive(false);
+	}
+
+	public void ButtonPortraitOnClick()
+	{
+		Screen.orientation = ScreenOrientation.Portrait;
+		landscapeDisp.SetActive(false);
+		portraitDisp.SetActive(true);
+	}
+
+	void OnRectTransformDimensionsChange()
+	{
+		textOrientation.text = Input.deviceOrientation.ToString();	
+	}
+}
---
+++ Assets/Scripts/Singleton/DeviceOrientation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeviceOrientation : MonoBehaviour {
+	private static DeviceOrientation instance = null;
+	public static DeviceOrientation Instance{
+		get{ return instance; }
+	}
+
+	void Awake()
+	{
+		if(instance != null && instance != this) Destroy(gameObject);
+		else instance = this;
+		
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+	IEnumerator Start()
+	{
+		while(true) {
+			
+			if(Screen.orientation != ScreenOrientation.Portrait){
+				if( Input.deviceOrientation == UnityEngine.DeviceOrientation.LandscapeLeft && Screen.orientation != ScreenOrientation.LandscapeLeft )
+					Screen.orientation = ScreenOrientation.LandscapeLeft;
+				if( Input.deviceOrientation == UnityEngine.DeviceOrientation.LandscapeRight && Screen.orientation != ScreenOrientation.LandscapeRight )
+					Screen.orientation = ScreenOrientation.LandscapeRight;
+			}
+
+			yield return new WaitForSeconds(0.5f);
+		}
+	}
+
+
+	public void SetPortrait()
+	{
+		Screen.orientation = ScreenOrientation.Portrait;
+	}
+
+	public void SetLandscape()
+	{
+		if(Input.deviceOrientation == UnityEngine.DeviceOrientation.LandscapeLeft)
+			Screen.orientation = ScreenOrientation.LandscapeLeft;
+		else 
+			Screen.orientation = ScreenOrientation.LandscapeRight;
+	}
+}
